49177aa591805bed8c92ded6e90ec22b3d8e0c5c
Tejas Sawant <tejas456sawant@gmail.com>
2022-07-30 18:31:12 +0530

    PlayerById Updated

--- controller/player-controller.go
+++ controller/player-controller.go
@@ -2,13 +2,29 @@ package controller
 
 import (
 	"context"
+	"encoding/json"
+	"log"
 	"net/http"
 	"time"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+type player struct {
+	cricinfo_id     string
+	identifier      string
+	identifier_name string
+	player          interface{}
+	content         struct {
+		teams          []interface{}
+		profile        interface{}
+		careerAverages interface{}
+		topRecords     []interface{}
+	}
+}
 
 func GetPlayer() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -56,7 +72,66 @@ func GetPlayerById() gin.HandlerFunc {
 		doc.Decode(&result)
 
 		if result == nil {
-			c.JSON(http.StatusNotFound, gin.H{"message": "Player not found."})
+			res, err := http.Get("https://www.espncricinfo.com/ci/content/player/" + id + ".html")
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"message": "internal server error"})
+				return
+			}
+			if res.StatusCode != 200 {
+				c.JSON(http.StatusNotFound, gin.H{"message": "Player not found."})
+				return
+			}
+			defer res.Body.Close()
+
+			pageDoc, err := goquery.NewDocumentFromReader(res.Body)
+			if err != nil {
+				log.Print(err)
+			}
+
+			script := pageDoc.Find("script#__NEXT_DATA__").Text()
+
+			var player_data map[string]map[string]map[string]map[string]map[string]interface{}
+			json.Unmarshal([]byte(script), &player_data)
+
+			var content map[string]map[string]map[string]map[string]map[string][]interface{}
+			var content2 map[string]map[string]map[string]map[string]map[string]interface{}
+
+			json.Unmarshal([]byte(script), &content)
+			json.Unmarshal([]byte(script), &content2)
+
+			player := player{
+				cricinfo_id:     id,
+				identifier:      "",
+				identifier_name: player_data["props"]["appPageProps"]["data"]["player"]["name"].(string),
+			}
+
+			player.player = player_data["props"]["appPageProps"]["data"]["player"]
+
+			player.content.teams = content["props"]["appPageProps"]["data"]["content"]["teams"]
+			player.content.profile = content2["props"]["appPageProps"]["data"]["content"]["profile"]
+			player.content.careerAverages = content2["props"]["appPageProps"]["data"]["content"]["careerAverages"]
+			player.content.topRecords = content["props"]["appPageProps"]["data"]["content"]["topRecords"]
+
+			insertDoc := bson.D{
+				{Key: "cricinfo_id", Value: player.cricinfo_id},
+				{Key: "identifier", Value: player.identifier},
+				{Key: "identifier_name", Value: player.identifier_name},
+				{Key: "player", Value: player.player},
+				{Key: "content", Value: bson.D{
+					{Key: "teams", Value: player.content.teams},
+					{Key: "profile", Value: player.content.profile},
+					{Key: "careerAverages", Value: player.content.careerAverages},
+					{Key: "topRecords", Value: player.content.topRecords},
+				}},
+			}
+
+			PlayerCollection.InsertOne(ctx, insertDoc)
+
+			var result bson.M
+			doc = PlayerCollection.FindOne(ctx, bson.M{"cricinfo_id": id})
+			doc.Decode(&result)
+
+			c.JSON(http.StatusOK, result)
 		} else {
 			c.JSON(http.StatusOK, result)
 		}
